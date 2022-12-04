32df3c6904efd631d61d0ec66315fa96d315774f
lilski190 <95133564+lilski190@users.noreply.github.com>
2022-12-04 20:09:27 +0100

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -87,6 +87,8 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 #}
 
 # Database Production
+DATABASES = {
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
