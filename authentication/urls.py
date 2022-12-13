23020e4b34b04d9b8122559212ecee35afe9098e
s84559 <lilidrabi@outlook.de>
2022-12-13 13:05:00 +0100

    some changes

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('token/revoke/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist'),
     path('user/password/<int:pk>/', ChangePasswordView.as_view(), name="change_password"),
     path('user/data/<int:pk>/', UpdateProfileView.as_view(), name="change_data"),
+    #TODO: get user data
 ]
