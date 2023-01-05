336e1b4c3695373507eda9577a8804c2f916f03a
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-01-05 14:33:09 +0100

    change database

--- Backend/settings.py
+++ Backend/settings.py
@@ -80,17 +80,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Backend.wsgi.application'
 
 # Database Develpopment
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Database Production
-DATABASES = {
-}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+#DATABASES = {
+#}
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
