ac0e4a4a55b062af333bfd1993ffd51338139fa7
s84559 <lilidrabi@outlook.de>
2023-01-14 14:34:51 +0100

    fix migrations

--- get_outside/models/commentsModel.py
+++ get_outside/models/commentsModel.py
@@ -1,12 +1,21 @@
 import uuid
+from random import randrange
 
 from django.db import models
 from authentication.models import CustomUser
 from get_outside.models.mappointModel import Mappoint
 
 
+def key_generator():
+    key = randrange(100, 100000)
+    if CustomUser.objects.filter(id=key).exists():
+        key = key_generator()
+    return key
+
+
 class Comment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.IntegerField(primary_key=True, default=key_generator, unique=True)
     mappoint_id = models.ForeignKey(Mappoint, related_name='comments', default=0, on_delete=models.CASCADE)
     author_id = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)  # oder SET() ???
     created_at = models.DateTimeField(auto_now=True)
