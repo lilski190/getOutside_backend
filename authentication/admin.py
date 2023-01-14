48ad173190597e9d3afe280b6e4bc776df47b9cc
lilski190 <95133564+lilski190@users.noreply.github.com>
2023-01-14 13:13:33 +0100

    add models to admin view

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from authentication.models import CustomUser
 
+from get_outside.models.mappointModel import Mappoint, Images
+from get_outside.models.categoryModel import Category
+from get_outside.models.favoritesModel import FavoritePins
+from get_outside.models.commentsModel import Comment
+
 
 class CustomUserAdmin(admin.ModelAdmin):
     model = CustomUser
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Mappoint)
+admin.site.register(Images)
+admin.site.register(Category)
+admin.site.register(FavoritePins)
+admin.site.register(Comment)
