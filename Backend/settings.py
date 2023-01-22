f4909c777724304c7654f6be7a8aa2c0a8e81065
lilski190 <95133564+lilski190@users.noreply.github.com>
2023-01-22 22:23:44 +0100

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', }}
 
 # Database Production
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -101,6 +104,7 @@ DATABASES = {
         'PORT': env('PORT'),
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
