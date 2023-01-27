84a6f91253b1370374aa07741b867af986582297
Josefine Hoppe <jojofine@web.de>
2023-01-27 15:50:55 +0100

    added 'Ratings' to admin.py and third ratings url

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from authentication.models import CustomUser
 
-from get_outside.models.mappointModel import Mappoint, Images
+from get_outside.models.mappointModel import Mappoint, Images, Ratings
 from get_outside.models.categoryModel import Category
 from get_outside.models.favoritesModel import FavoritePins
 from get_outside.models.commentsModel import Comment
@@ -17,3 +17,4 @@ admin.site.register(Images)
 admin.site.register(Category)
 admin.site.register(FavoritePins)
 admin.site.register(Comment)
+admin.site.register(Ratings)
